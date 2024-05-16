3cb05d2f5594b41804f335a65077f6ab3ff0d4f7
Robin <160778136+robincse-usa16@users.noreply.github.com>
2024-05-16 13:57:28 +0600

    Add files via upload

---
+++ src/component/footer/Footer.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+    FaFacebookF,
+    FaInstagram,
+    FaTwitter,
+    FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <ContentWrapper>
+                <ul className="menuItems">
+                    <li className="menuItem">Terms Of Use</li>
+                    <li className="menuItem">Privacy-Policy</li>
+                    <li className="menuItem">About</li>
+                    <li className="menuItem">Blog</li>
+                    <li className="menuItem">FAQ</li>
+                </ul>
+                <div className="infoText">
+            🎬 Dive into a world of cinematic brilliance at Live Movies. Discover the latest blockbuster movies and trending TV shows, all meticulously curated for your entertainment. From heartwarming dramas to pulse-pounding action, find your next obsession on our sleek and user-friendly platform. Elevate your streaming experience with us where every click unlocks a new adventure. Join us at Live Movies and let the movie magic begin! 🍿✨ <br/>Kings World Live Movie
+                </div>
+                <div className="socialIcons">
+                    <span className="icon">
+                        <FaFacebookF />
+                    </span>
+                    <span className="icon">
+                        <FaInstagram />
+                    </span>
+                    <span className="icon">
+                        <FaTwitter />
+                    </span>
+                    <span className="icon">
+                        <FaLinkedin />
+                    </span>
+                </div>
+            </ContentWrapper>
+            
+        </footer>
+    );
+};
+
+export default Footer;
